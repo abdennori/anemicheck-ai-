caa3b5faa90dfc1513c3ba2621c90b0325d72cc1
abdennori <abdennoursoualah5@gmail.com>
2026-07-02 22:52:36 +0100

    Fix app loader

--- app.py
+++ app.py
@@ -360,6 +360,7 @@ def t(key):
 
 # ========== CSS MODERN (exact variables from image.png) ==========
 st.markdown("""
+<style>
     @import url('https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=Plus+Jakarta+Sans:wght@500;600;700;800&display=swap');
 
     * { box-sizing: border-box; margin: 0; padding: 0; }
